3a8a08ac48e8ac1f9290434860f87df20c55d895
Omotayo Olaniyan <olantayo3627@gmail.com>
2022-11-07 22:13:47 +0100

    fix : add media type to accomodate different media types

--- src/pages/Hero/Hero.js
+++ src/pages/Hero/Hero.js
@@ -2,21 +2,29 @@ import React, { useState, useEffect, useCallback } from 'react'
 import '../Hero/Hero.scss'
 import axios from 'axios'
 import moment from 'moment'
+import hero_loader from '../../assets/hero-loader.png'
 
 const Hero = () => {
   // Endpoint to get the hero date and image
   const baseURL = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}`
 
   const [heroData, setHeroData] = useState({})
+  const [loading, setLoading] = useState(true)
 
+  // Get hero data which includes the date and the picture of the day from NASA
   const handleGetHeroData = useCallback(async () => {
     try {
       let response = await axios.get(baseURL)
       setHeroData({
         image: response.data.hdurl,
+        url: response.data.url,
         date: response.data.date,
+        media_type: response.data.media_type,
       })
-    } catch (error) {}
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+    }
   }, [baseURL])
 
   useEffect(() => {
@@ -25,16 +33,33 @@ const Hero = () => {
 
   return (
     <div className="hero-container">
-      <h1>
-        <span>NASA: </span>
-        Picture of the Day
+      {/* Display loading image if data is being retrieved  */}
+      {loading ? (
+        <img src={hero_loader} alt="Hero" title="Loading" />
+      ) : (
         <div>
-          {moment(heroData.date).format('dddd') +
-            ', ' +
-            moment(heroData.date).format('LL')}
+          {/* Display loading image if the data media type is a video */}
+          {heroData.media_type === 'video' ? (
+            <>
+              <img src={hero_loader} alt="Hero" title="Loading" />
+            </>
+          ) : (
+            <>
+              {/* Display picture of the day and date if the data media type is an image */}
+              <h1>
+                <span>NASA: </span>
+                Picture of the Day
+                <div>
+                  {moment(heroData.date).format('dddd') +
+                    ', ' +
+                    moment(heroData.date).format('LL')}
+                </div>
+              </h1>
+              <img src={heroData.image} alt="Hero" />
+            </>
+          )}
         </div>
-      </h1>
-      <img src={heroData.image} alt="Hero" />
+      )}
     </div>
   )
 }
